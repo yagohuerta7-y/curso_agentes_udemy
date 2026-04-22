e061d8c594c5a991869d1600aee0bc0b8597cb97
Yago <yagoraulhuertaorozco@gmail.com>
2026-04-21 21:59:17 -0600

    Se hizo un cambio con clases mas generales para mejorar la limpieza del código

--- Seccion3/Scripts/interfaz.py
+++ Seccion3/Scripts/interfaz.py
@@ -28,14 +28,16 @@ st.title("Tarea de la sección 3: IAgo")
 
 # Configuramos el session state (para guardar el historia en una especie de caché)
 if "messages" not in st.session_state:
-    st.session_state.messages = []
+    # Si no hay mensajes en el session state, inicializamos la conversación con un mensaje del sistema
+    st.session_state.messages = [
+        SystemMessage(content="Eres un asistente llamado IAgo, responde a las preguntas que te haga el usuario.")
+    ]
     
 
 # Hacemos el prompt template
 plantilla = PromptTemplate(
     input_variables = ["mensajes", "pregunta"],
     template = """
-    Eres un asistente llamado IAgo, responde a las preguntas que te haga el usuario.
     El historial de la conversación es el siguiente:
     {mensajes}
     
@@ -47,28 +49,37 @@ plantilla = PromptTemplate(
 
 # Mostramos los mensajes en la página
 for message in st.session_state.messages:
+    
+    # Si es un mensaje del sistema, no lo mostramos 
+    if isinstance(message, SystemMessage):
+        continue
+    
+    # Obtenemos el rol del mensaje (assistant o user) y lo mostramos en la página
+    role = "assistant" if isinstance(message, AIMessage) else "user"
+    
+    # Mostramos el mensaje
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
+        
         
         
 # Hacemos la lógica de los mensajes
 if prompt := st.chat_input("¿En que te puedo ayudar hoy?"):
     
-    # 1. Mostramos el mensaje que puso el usuario
+    # 1. Mostramos el mensaje que puso el usuario y lo guardamos en el session state
     with st.chat_message("user"):
         st.markdown(prompt)
         
-    # 2. Añadimos el mensaje del usuario al historial del chat
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.messages.append(HumanMessage(content=prompt)) # Antes esto era st.session_state.messages.append({"role": "user", "content": prompt})
     
 
-    # 3. Hacemos la cadena (solo cuando hay un mensaje de parte del usuario)
+    # 2. Hacemos la cadena (solo cuando hay un mensaje de parte del usuario)
     chain = plantilla | llm
 
-    # 4. Generamos la respuesta
+    # 3. Generamos la respuesta
     response = chain.invoke({"mensajes": st.session_state.messages, "pregunta": prompt})
 
-    # 5. Mostramos y guardamos la respuesta del modelo
+    # 4. Mostramos y guardamos la respuesta del modelo
     with st.chat_message("assistant"):
         st.markdown(response.content)
-        st.session_state.messages.append({"role": "assistant", "content": response.content})
+        st.session_state.messages.append(AIMessage(content=response.content)) # Antes esto era st.session_state.messages.append({"role": "assistant", "content": response.content})
